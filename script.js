19e7f3d779156713fc3c450db11a19c036fd83c1
Kakasi Daniel <daniel_kksi@Kakasis-iMac.local>
2020-07-23 15:11:32 +0300

    optimized code

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
-const UI = (function(){
-  
-  document.querySelector('.dropdown_btn').addEventListener('mouseleave', () => {
-    document.querySelector('.dropdown').style.visibility = 'hidden';
-    document.querySelector('.dropdown').style.opacity = 0;
-  });
+const UI = (function () {
 
-  document.querySelector('.dropdown').addEventListener('mouseenter', e => {
-    document.querySelector('.dropdown').style.visibility = 'visible';
-    document.querySelector('.dropdown').style.opacity = 1;
-  });
-
-  document.querySelector('.dropdown').addEventListener('mouseleave', e => {
-    document.querySelector('.dropdown').style.visibility = 'hidden';
-    document.querySelector('.dropdown').style.opacity = 0;
-  });
-
-  document.querySelectorAll('.dropdown_btn').forEach(e =>{
-    e.addEventListener('mouseenter', () => {
+  const showDropdown = function (d) {
+    if (d === 1) {
       document.querySelector('.dropdown').style.visibility = 'visible';
       document.querySelector('.dropdown').style.opacity = 1;
-    });
-  });
-
-  document.querySelector('.dropdown_btn2').addEventListener('mouseleave', () => {
-    document.querySelector('.dropdown2').style.visibility = 'hidden';
-    document.querySelector('.dropdown2').style.opacity = 0;
-  });
-
-  document.querySelector('.dropdown2').addEventListener('mouseenter', e => {
-    document.querySelector('.dropdown2').style.visibility = 'visible';
-    document.querySelector('.dropdown2').style.opacity = 1;
-  });
-
-  document.querySelector('.dropdown2').addEventListener('mouseleave', e => {
-    document.querySelector('.dropdown2').style.visibility = 'hidden';
-    document.querySelector('.dropdown2').style.opacity = 0;
-  });
-
-  document.querySelectorAll('.dropdown_btn2').forEach(e =>{
-    e.addEventListener('mouseenter', () => {
+    } else {
       document.querySelector('.dropdown2').style.visibility = 'visible';
       document.querySelector('.dropdown2').style.opacity = 1;
+    }
+  };
+
+  const hideDropdown = function (d) {
+    if (d === 1) {
+      document.querySelector('.dropdown').style.visibility = 'hidden';
+      document.querySelector('.dropdown').style.opacity = 0;
+    } else {
+      document.querySelector('.dropdown2').style.visibility = 'hidden';
+      document.querySelector('.dropdown2').style.opacity = 0;
+    }
+  };
+
+  document.querySelector('.dropdown_btn').addEventListener('mouseenter', () => {
+    showDropdown(1);
+  });
+  document.querySelector('.dropdown_btn').addEventListener('mouseleave', () => {
+    hideDropdown(1);
+  });
+  document.querySelector('.dropdown').addEventListener('mouseenter', (e) => {
+    showDropdown(1);
+  });
+  document.querySelector('.dropdown').addEventListener('mouseleave', (e) => {
+    hideDropdown(1);
+  });
+
+  document
+    .querySelector('.dropdown_btn2')
+    .addEventListener('mouseenter', () => {
+      showDropdown(2);
     });
+  document
+    .querySelector('.dropdown_btn2')
+    .addEventListener('mouseleave', () => {
+      hideDropdown(2);
+    });
+  document.querySelector('.dropdown2').addEventListener('mouseenter', (e) => {
+    showDropdown(2);
+  });
+  document.querySelector('.dropdown2').addEventListener('mouseleave', (e) => {
+    hideDropdown(2);
   });
 
   window.onscroll = () => {
-    if(pageYOffset > innerHeight /2 ){
+    if (pageYOffset > innerHeight / 2) {
       document.querySelector('.nav_2').style.top = 0;
       document.querySelector('.hamburger_btn').style.top = '88%';
-    }else{
+    } else {
       document.querySelector('.nav_2').style.top = '-100px';
-      document.querySelector('.dropdown2').style.visibility = 'hidden';
-      document.querySelector('.dropdown2').style.opacity = 0;
+      hideDropdown(2);
       document.querySelector('.hamburger_btn').style.top = '5%';
     }
-  }
+  };
 })();
-
